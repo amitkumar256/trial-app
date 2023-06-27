8782055f9ada4449ac88cfe2a36abc8a2ddb7422
amit <amit256375@gmail.com>
2023-06-27 14:39:04 +0530

    ...

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
+
 if (
   process.env.LD_LIBRARY_PATH == null ||
   !process.env.LD_LIBRARY_PATH.includes(
     `${process.env.PWD}/node_modules/canvas/build/Release:`,
   )
 ) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+  const additionalPath = `${process.env.PWD}/node_modules/canvas/build/Release`;
+  process.env.LD_LIBRARY_PATH = [
+    additionalPath,
+    process.env.LD_LIBRARY_PATH || '',
+  ].join(':');
 }
-module.exports = nextConfig
+
+module.exports = nextConfig;
